253bcf29ec4a7f54bb71f8677717ef9dd434bf1e
Manuel A. Rivas <mrivas@stanford.edu>
2020-02-12 23:31:49 -0500

    Update plotregularizedMR.R

--- plotregularizedMR.R
+++ plotregularizedMR.R
@@ -7,10 +7,10 @@ lablist<-vars[xts$theta0 > 0]
 
  plot(row(xts$theta0)[xts$theta0 > 0],xts$theta0[xts$theta0 > 0], xlab = "Pleiotropic variants\nDiscarded instruments", ylab = "Pleiotropy estimates")
 axis(1, at=row(xts$theta0)[xts$theta0 > 0], labels = FALSE)
-text(row(xts$theta0)[xts$theta0 > 0], par("usr")[3] - 0.2, labels = lablist, srt = 45, pos = 1, xpd = TRUE)
- lablst <- phenos
+text(row(xts$theta0)[xts$theta0 > 0], par("usr")[3], labels = lablist, srt = 45, pos = 1, xpd = TRUE)
+ lablst2 <- phenos
  plot(row(xts$thetaL),xts$thetaL, xlab = "Phenotype", ylab = "Causal effect estimate log(OR)/SD")
  axis(1, at=row(xts$thetaL), labels = FALSE)
-text(row(xts$thetaL), par("usr")[3] - 0.2, labels = lablist, srt = 45, pos = 1, xpd = TRUE)
+text(row(xts$thetaL), par("usr")[3], labels = lablst2, srt = 45, pos = 1, xpd = TRUE)
 
 }
