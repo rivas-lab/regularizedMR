b138c9951b0e7c09de63659538668ce4bca0c407
Dr. Manuel A Rivas <mrivas@sh01-ln04.stanford.edu>
2020-02-15 06:54:13 -0800

    plots with output variable

--- plotregularizedMR.R
+++ plotregularizedMR.R
@@ -3,14 +3,14 @@ plotMR <- function(output,phenos, vars){
 #vars should be of length M - number of variants
 par(mfrow = c(2,1))
 par(xaxt="n")
-lablist<-vars[xts$theta0 > 0]
+lablist<-vars[output$theta0 > 0]
 
- plot(row(xts$theta0)[xts$theta0 > 0],xts$theta0[xts$theta0 > 0], xlab = "Pleiotropic variants\nDiscarded instruments", ylab = "Pleiotropy estimates")
-axis(1, at=row(xts$theta0)[xts$theta0 > 0], labels = FALSE)
-text(row(xts$theta0)[xts$theta0 > 0], par("usr")[3], labels = lablist, srt = 45, pos = 1, xpd = TRUE)
+ plot(row(output$theta0)[output$theta0 > 0],output$theta0[output$theta0 > 0], xlab = "Pleiotropic variants\nDiscarded instruments", ylab = "Pleiotropy estimates")
+axis(1, at=row(output$theta0)[output$theta0 > 0], labels = FALSE)
+text(row(output$theta0)[output$theta0 > 0], par("usr")[3], labels = lablist, srt = 45, pos = 1, xpd = TRUE)
  lablst2 <- phenos
- plot(row(xts$thetaL),xts$thetaL, xlab = "Phenotype", ylab = "Causal effect estimate log(OR)/SD")
- axis(1, at=row(xts$thetaL), labels = FALSE)
-text(row(xts$thetaL), par("usr")[3], labels = lablst2, srt = 45, pos = 1, xpd = TRUE)
+ plot(row(output$thetaL),output$thetaL, xlab = "Phenotype", ylab = "Causal effect estimate log(OR)/SD")
+ axis(1, at=row(output$thetaL), labels = FALSE)
+text(row(output$thetaL), par("usr")[3], labels = lablst2, srt = 45, pos = 1, xpd = TRUE)
 
 }
